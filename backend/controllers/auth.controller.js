864f9bd9c8b1a73379349fc99b6064d3c71044e3
Houssem Tabboubi <124980612+tb7ssem@users.noreply.github.com>
2024-10-07 12:53:04 +0100

    authentication complit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
@@ -50,14 +51,105 @@ export const signup = async (req, res) => {
 
     setCookies(res, accessToken, refreshToken);
 
-    res.status(201).json({ user, message: "User created successfully" });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
   } catch (error) {
+    console.log("Error in signup", error.message);
     res.status(500).json({ message: error.message });
   }
 };
 export const login = async (req, res) => {
-  res.send("login route called");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateToken(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.log("Error in login", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 export const logout = async (req, res) => {
-  res.send("logout route called");
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(400).json({ message: "No refresh token provided" });
+    }
+
+    console.log("Received refresh token:", refreshToken);
+
+    let decoded;
+    try {
+      decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    } catch (err) {
+      return res
+        .status(401)
+        .json({ message: "Invalid refresh token", error: err.message });
+    }
+
+    const result = await redis.del(`refresh_token:${decoded.userId}`);
+    if (result === 0) {
+      console.log("No token found in Redis for user:", decoded.userId);
+    } else {
+      console.log("Token deleted from Redis for user:", decoded.userId);
+    }
+
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
+
+export const refreshToken = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return res.status(401).json({ message: "No refresh token provided" });
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+    if (refreshToken !== storedToken) {
+      return res.status(401).json({ message: "Invalid refresh token" });
+    }
+
+    const accessToken = jwt.sign(
+      { userId: decoded.userId },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+    res.status(200).json({ message: "Token refreshed successfully" });
+  } catch (error) {
+    console.log("Error in refreshToken", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//to do later get profile
+//export const getprofile = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 
@@ -9,8 +11,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
